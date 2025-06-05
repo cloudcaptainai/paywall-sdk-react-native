b57cd7c468eb9bea88a6415310dae4053498732e
Kyle Gorlick <kyle@kylegorlick.com>
2025-06-04 17:25:34 -0700

    fix: open/close events for fallback paywalls

--- ios/HeliumSwiftInterface.swift
+++ ios/HeliumSwiftInterface.swift
@@ -291,4 +291,14 @@ class HeliumBridge: RCTEventEmitter {
   public func hideAllUpsells() {
     Helium.shared.hideAllUpsells();
   }
+
+  @objc
+  public func fallbackOpenOrCloseEvent(
+    trigger: String?,
+    isOpen: Bool,
+    viewType: String?
+  ) {
+    HeliumPaywallDelegateWrapper.shared.onFallbackOpenCloseEvent(trigger: trigger, isOpen: isOpen, viewType: viewType)
+  }
+
 }

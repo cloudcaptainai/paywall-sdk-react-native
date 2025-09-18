435ec67d43e53dfa2c5dda48f23acd01938c06e2
Kyle Gorlick <kyle@kylegorlick.com>
2025-09-18 00:02:40 -0700

    fix: product id on purchase succeeded event handler

--- src/native-interface.tsx
+++ src/native-interface.tsx
@@ -212,7 +212,7 @@ function callPaywallEventHandlers(event: HeliumPaywallEvent) {
       case 'purchaseSucceeded':
         paywallEventHandlers?.onPurchaseSucceeded?.({
           type: 'purchaseSucceeded',
-          productId: event.productKey ?? 'unknown',
+          productId: event.productId ?? 'unknown',
           triggerName: event.triggerName ?? 'unknown',
           paywallName: event.paywallName ?? 'unknown',
           isSecondTry: event.isSecondTry ?? false,
